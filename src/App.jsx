8ae5ab666367055b796fe279521acca5eb4f578e
alkeshmahamune <alkeshmahamune@gmail.com>
2025-05-30 14:50:41 +0530

    added to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ManomilanLogin from './components/ManomilanLogin.jsx';
+import ManomilanLogin from './components/login.jsx';
 import MultiStepRegistrationForm from './components/multistepForm.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<ManomilanLogin />} /> */}
-        {/* <Route path="/" element={<MultiStepRegistrationForm />} /> */}
+        <Route path="/" element={<ManomilanLogin />} />
       </Routes>
     </Router>
   )
